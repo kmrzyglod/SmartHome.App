a18d36fc951b987cc452d73b2138c043b33642ec
Kamil Mrzyglod <kamas514@gmail.com>
2020-03-04 20:57:01 +0100

    Device error event implemented

---
+++ src/Application/SmartHome.Application/Events/Devices/Shared/Error/ErrorEvent.cs
@@ -0,0 +1,12 @@
+﻿using SmartHome.Application.Enums;
+using SmartHome.Application.Interfaces.Event;
+
+namespace SmartHome.Application.Events.Devices.Shared.Error
+{
+    public class ErrorEvent: IEvent
+    {
+        public string Message { get; set; } = string.Empty;
+        public ErrorEventLevel ErrorLevel { get; set; }
+        public string Source { get; set; } = string.Empty;
+    }
+}
