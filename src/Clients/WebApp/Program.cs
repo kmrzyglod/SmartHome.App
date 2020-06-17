75e1e62e5c2e8e3902ac89b792ec35cc72aff855
Kamil Mrzyglod <kamas514@gmail.com>
2020-06-17 14:31:44 +0200

    Update packages versions

--- src/Clients/WebApp/Program.cs
+++ src/Clients/WebApp/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Text;
-using Microsoft.AspNetCore.Blazor.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 using MatBlazor;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SmartHome.Application.Shared.Interfaces.DateTime;
 using SmartHome.Clients.WebApp.Services.Analytics;
 using SmartHome.Clients.WebApp.Services.EventLog;
@@ -20,10 +21,14 @@ namespace SmartHome.Clients.WebApp
         {
            var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+            builder.Services.AddTransient(sp =>
+                new HttpClient
+                {
+                    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+                });
+
             builder.Services.AddLoadingBar();
             builder.Services.AddDevExpressBlazor();
-          
-            builder.Services.AddLoadingBar();
             builder.Services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
             builder.Services.AddSingleton<IApiClient, ApiClient>();
             builder.Services.AddSingleton<IWeatherService, WeatherService>();
