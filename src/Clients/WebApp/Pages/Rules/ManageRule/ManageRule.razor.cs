2bae3e67461b24477669e1832347f62f52ecaeea
Kamil Mrzyglod <kamas514@gmail.com>
2021-08-09 19:45:42 +0200

    Rule management views added

---
+++ src/Clients/WebApp/Pages/Rules/ManageRule/ManageRule.razor.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Radzen;
+using SmartHome.Application.Shared.Interfaces.DateTime;
+using SmartHome.Clients.WebApp.Services.Shared.CommandsExecutor;
+using SmartHome.Clients.WebApp.Shared.Components;
+
+namespace SmartHome.Clients.WebApp.Pages.Rules.ManageRule
+{
+    public class ManageRuleComponent : ComponentWithNotificationHub
+    {
+        [Inject] protected IDateTimeProvider DateTimeProvider { get; set; } = null!;
+
+        [Inject] protected ICommandsExecutor CommandsExecutor { get; set; } = null!;
+
+        [Inject] protected NotificationService ToastrNotificationService { get; set; } = null!;
+        [Inject] protected IJSRuntime JsRuntime { get; set; } = null!;
+
+        [Parameter] public int? RuleId { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+        }
+
+        protected async Task OnStepChange(int index)
+        {
+            await Task.Delay(5);
+            if (index == 1)
+            {
+                await JsRuntime.InvokeVoidAsync("initQueryBuilder", "query-builder");
+            }
+        }
+    }
+}
---
+++ src/Clients/WebApp/Pages/Rules/RulesList/RulesList.razor.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+using SmartHome.Application.Shared.Interfaces.DateTime;
+using SmartHome.Clients.WebApp.Services.Shared.CommandsExecutor;
+using SmartHome.Clients.WebApp.Shared.Components;
+
+namespace SmartHome.Clients.WebApp.Pages.Rules.RulesList
+{
+    public class RulesListComponent : ComponentWithNotificationHub
+    {
+        [Inject] protected IDateTimeProvider DateTimeProvider { get; set; } = null!;
+
+        [Inject] protected ICommandsExecutor CommandsExecutor { get; set; } = null!;
+
+        [Inject] protected NotificationService ToastrNotificationService { get; set; } = null!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+        }
+    }
+}
